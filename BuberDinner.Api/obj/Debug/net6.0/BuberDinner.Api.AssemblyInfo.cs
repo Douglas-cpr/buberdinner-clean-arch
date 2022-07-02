34343196a6ba6e7a14bc267bd01ba9515b44a8a2
Douglas <douglas-cprdev@hotmail.com>
2022-07-02 15:42:15 -0300

    config: turn jwt configs to secret

--- BuberDinner.Api/obj/Debug/net6.0/BuberDinner.Api.AssemblyInfo.cs
+++ BuberDinner.Api/obj/Debug/net6.0/BuberDinner.Api.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("251f3136-6590-491f-a97d-dfb33824cc09")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
