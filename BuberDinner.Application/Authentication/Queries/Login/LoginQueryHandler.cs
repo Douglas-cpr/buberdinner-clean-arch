6fec8fa5811ff5365dfca82699d4e7977ef0a41c
Douglas <douglas-cprdev@hotmail.com>
2022-07-13 23:09:49 -0300

    feat add mediatr

--- BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -1,32 +1,34 @@
 using BuberDinner.Application.Common.Interfaces.Authentication;
 using BuberDinner.Application.Common.Interfaces.Persistence;
 using BuberDinner.Application.Services.Authentication.Common;
-using BuberDinner.Domain.Common.Errors;
 using BuberDinner.Domain.Entities;
+using BuberDinner.Domain.Common.Errors;
 using ErrorOr;
+using MediatR;
+using BuberDinner.Application.Authentication.Queries.Login;
 
-namespace BuberDinner.Application.Services.Authentication.Queries;
+namespace BuberDinner.Application.Authentication.Commands.Register;
 
-public class AuthenticationQueryService : IAuthenticationQueryService
+public class LoginCommandHandler : 
+  IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
 {
   private readonly IJwtTokenGenerator _jwtTokenGenerator;
   private readonly IUserRepository _userRepository;
 
-  public AuthenticationQueryService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+  public LoginCommandHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
   {
     _jwtTokenGenerator = jwtTokenGenerator;
     _userRepository = userRepository;
   }
 
-  public ErrorOr<AuthenticationResult> Login(string email, string password)
+  public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery query, CancellationToken cancellationToken)
   {
-
-    if (_userRepository.GetUserByEmail(email) is not User user)
+     if (_userRepository.GetUserByEmail(query.Email) is not User user)
     {
       return Erros.Authentication.InvalidCredentials;
     }
 
-    if (user.Password != password)
+    if (user.Password != query.Password)
     {
       return Erros.Authentication.InvalidCredentials;
     }
